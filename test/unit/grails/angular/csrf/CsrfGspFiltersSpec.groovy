6da90ff4ad7d3de2211eede132fa8cc6d3177191
craigatk <atk.craig@gmail.com>
2014-08-09 10:45:19 -0500

    Adding test missed in last commit

---
+++ test/unit/grails/angular/csrf/CsrfGspFiltersSpec.groovy
@@ -0,0 +1,32 @@
+package grails.angular.csrf
+
+import grails.angular.csrf.api.PersonController
+import grails.test.mixin.Mock
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+@Mock([CsrfFilters, CsrfService])
+@TestFor(HomeController)
+class CsrfGspFiltersSpec extends Specification {
+    void 'should add token to session'() {
+        setup:
+        defineBeans {
+            csrfService(CsrfNonApiService)
+        }
+
+        when:
+        withFilters(controller: "*", action: "*") {
+            controller.index()
+        }
+
+        then:
+        assert session['csrfToken']
+    }
+}
+
+class CsrfNonApiService extends CsrfService {
+    @Override
+    boolean isApiController(String controllerName) {
+        return false
+    }
+}
